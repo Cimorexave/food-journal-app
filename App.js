27e0b3dc6cc69c4524976aaa83893ec50b3a8e3c
Cimorexave <sadeq.s502@gmail.com>
2022-05-29 16:42:36 +0430

    added initial files

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.text}>Test Text</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'darkgreen',
+    flex: 1,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'grey',
+  },
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'grey',
+  },
+  header: {
+    flex: 1,
+    marginTop: 20,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'grey',
+  },
+  text: {
+    color: 'green',
+    fontSize:20,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'grey',
+  }
+});
